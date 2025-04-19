e71a461369023d823e042d83326641c3df77e7a3
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-04-19 14:34:02 -0400

    Création de la barre de navigation qui se positionne sur le coté gauche de l'application

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import NavigationComponent from './components/static/NavigationComponent.vue'
 </script>
 
 <template>
   <header>
-    <h1>Hello, World !</h1>
+    <!-- <h1>Hello, World !</h1> -->
   </header>
+  <NavigationComponent />
 
   <RouterView />
 </template>
