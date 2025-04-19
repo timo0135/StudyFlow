3a305271f1567ab69b0aa151614319f837e4bbb5
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-04-19 15:06:01 -0400

    Création de la route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,14 @@
+import DashBoardView from '@/views/DashBoardView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashBoardView
+    }
   ],
 })
 
