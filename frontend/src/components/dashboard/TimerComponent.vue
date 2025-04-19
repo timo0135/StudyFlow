c4723e9daa879d60f17bdd1a7c24484441d8ced6
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-04-19 17:13:33 -0400

    Création d'un composant pour afficher le timer d'une tache en cours

---
+++ frontend/src/components/dashboard/TimerComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full h-full p-6 rounded-2xl bg-blue-100 flex flex-col justify-center">
+        <h3 class="text-xl font-medium mb-6 text-gray-700 ">Chronomètre de concentration</h3>
+        <div class="text-center text-7xl font-medium text-[#3a6bb5] flex-grow flex items-center justify-center border-b-2 border-gray-300 mb-6 ">
+            25:00
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
