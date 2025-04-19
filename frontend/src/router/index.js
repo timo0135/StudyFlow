539dac476209fddfd3f95938f774b58c8b51710b
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-04-19 16:18:57 -0400

    redirection du home vers le dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      redirect: '/dashboard'
+    },
+    {
       path: '/dashboard',
       name: 'dashboard',
       component: DashBoardView
