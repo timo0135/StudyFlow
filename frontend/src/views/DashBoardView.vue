8df33c07e15dddcb6850b345c1040d80b7076d30
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-04-19 17:30:45 -0400

    Insertion des composant dans la vue dashboard et les afficher sous forme de grille avec deux par ligne et colonnes

--- frontend/src/views/DashBoardView.vue
+++ frontend/src/views/DashBoardView.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
-        <h1>Hello, World!</h1>
+    <div class="grid grid-cols-2 h-full gap-10 p-10">
+        <TasksMinimalComponent class="w-full h-full" />
+        <ScheduleMinimalComponent class="w-full h-full" />
+        <TimerComponent class="w-full h-full" />
+        <StatsMinimalComponent class="w-full h-full" />
     </div>
 </template>
 
-<script>
-export default {
-    name: "DashBoardView",
-};
+<script setup>
+import ScheduleMinimalComponent from '@/components/dashboard/ScheduleMinimalComponent.vue';
+import StatsMinimalComponent from '@/components/dashboard/StatsMinimalComponent.vue';
+import TasksMinimalComponent from '@/components/dashboard/TasksMinimalComponent.vue';
+import TimerComponent from '@/components/dashboard/TimerComponent.vue';
+
 </script>
 
 <style scoped>
