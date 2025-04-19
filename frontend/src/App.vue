ee316c214a13ae7e1609d8889aabcf788fcf23a3
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2025-04-18 20:54:45 -0400

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Hello, World !</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
